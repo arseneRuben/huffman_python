9d008df4aeaed4bf000e4a9c48199ef9b1f619bb
arseneruben <fopoar@gmail.com>
2024-12-02 09:42:49 -0500

    test abat

--- test_huffman.py
+++ test_huffman.py
@@ -5,7 +5,7 @@ def testA():   # Compresser un fichier
     input_file = "exemple.txt"  # Fichier source
     compressed_file = "exemple_com.txt"  # Fichier compressé
     huffman.compress(input_file, compressed_file)
-    assert huffman.frequency_table['a']==2
+    assert huffman.frequency_table['a']==5
 
 def testB():   # Compresser un fichier
     huffman = HuffmanCoding()
